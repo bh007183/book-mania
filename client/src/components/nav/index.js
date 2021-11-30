a0fe3c341d446d88e7857eef4b8cb7528575a99c
Ben Hopkins <bjhops17@gmail.com>
2021-11-30 11:50:44 -0800

    added changes to header

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,38 +1,52 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import EmailIcon from '@material-ui/icons/Email';
-import GroupWorkIcon from '@material-ui/icons/GroupWork';
-import InfoIcon from '@material-ui/icons/Info';
-import { Hidden } from '@material-ui/core';
-import HomeIcon from '@material-ui/icons/Home';
-import {Link} from "react-router-dom";
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import EmailIcon from "@material-ui/icons/Email";
+import GroupWorkIcon from "@material-ui/icons/GroupWork";
+import InfoIcon from "@material-ui/icons/Info";
+import { Hidden } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import { Link } from "react-router-dom";
 
-import "./style.css"
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
+import "./style.css";
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    
+    display: "flex",
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
@@ -40,17 +54,16 @@ const useStyles = makeStyles((theme) => ({
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginRight: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
@@ -60,24 +73,24 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginRight: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -86,12 +99,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Nav() {
-  
- 
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
- 
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -101,54 +111,87 @@ export default function Nav() {
     setOpen(false);
   };
 
-  const label = { inputProps: { 'aria-label': 'Translate' } }
+  const label = { inputProps: { "aria-label": "Translate" } };
 
   return (
-    
     <header className={classes.root}>
       <CssBaseline />
-      <AppBar
-      >
+      <AppBar>
         <Toolbar>
-            <div id="logoContain">
-              
-        <Typography id="logoText" variant="h4" noWrap>
-        <Link name="home" to="/" style={{textDecoration: "none", color: "white"}}>
-         BookMania
-            </Link>
-          </Typography>
-         
+          <div id="logoContain">
+            <Typography id="logoText" variant="h4" noWrap>
+              <Link
+                name="home"
+                to="/"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                BookMania
+              </Link>
+            </Typography>
           </div>
           <div id="navLinksContain">
-          <Hidden smDown  >
-              <Link to="/dashboard"className="navLinks">Dashboard</Link>
-              <Link to="/browse"className="navLinks">Browse</Link>
-              <Link to="/manage-account"className="navLinks">Manage Account</Link>
-             
-            
+            <Hidden smDown>
+              <Link to="/dashboard" className="navLinks">
+                Dashboard
+              </Link>
+              <Link to="/browse" className="navLinks">
+                Browse
+              </Link>
 
-
-              
+              <FormControl sx={{ m: 1, width: 150, borderColore: "white" }}>
+                <InputLabel
+                  id="demo-multiple-name-label"
+                  style={{ color: "white" }}
+                >
+                  Account
+                </InputLabel>
+                <Select
+                  labelId="demo-multiple-name-label"
+                  id="demo-multiple-name"
+                  input={<OutlinedInput label="Name" />}
+                  MenuProps={MenuProps}
+                >
+                  <MenuItem>
+                    <Link
+                      to="/manage-account"
+                      style={{ color: "black", textDecoration: "none", width: "100%", height: "100%"}}
+                    >
+                      Manage Account
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link
+                      to="/notifications"
+                      style={{ color: "black", textDecoration: "none", width: "100%", height: "100%"}}
+                    >
+                      Notifications
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link
+                      to="/manage-connections"
+                      style={{ color: "black", textDecoration: "none", width: "100%", height: "100%"}}
+                    >
+                      Connect
+                    </Link>
+                  </MenuItem>
+                </Select>
+              </FormControl>
 
               {/* checked={language} */}
-              </Hidden>
-          <Hidden mdUp  >
-          
-          <IconButton
-           
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="end"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          </Hidden>
-
+            </Hidden>
+            <Hidden mdUp>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="end"
+                className={clsx(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Hidden>
           </div>
-          
-         
         </Toolbar>
       </AppBar>
       <Drawer
@@ -162,45 +205,63 @@ export default function Nav() {
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            {theme.direction === "ltr" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
         <List>
-        <Link name="home"onClick={handleDrawerClose} className="link" to="/">
-            <ListItem button >
-              <ListItemIcon> <HomeIcon className="icon" /></ListItemIcon>
+          <Link name="home" onClick={handleDrawerClose} className="link" to="/">
+            <ListItem button>
+              <ListItemIcon>
+                {" "}
+                <HomeIcon className="icon" />
+              </ListItemIcon>
               <ListItemText primary="Home" />
             </ListItem>
-            </Link>
-            <Divider />
-           <Link  name="whoWeAre" onClick={handleDrawerClose} className="link"to="/who-we-are">
-            <ListItem button >
-              <ListItemIcon> <InfoIcon className="icon"/></ListItemIcon>
+          </Link>
+          <Divider />
+          <Link
+            name="whoWeAre"
+            onClick={handleDrawerClose}
+            className="link"
+            to="/who-we-are"
+          >
+            <ListItem button>
+              <ListItemIcon>
+                {" "}
+                <InfoIcon className="icon" />
+              </ListItemIcon>
               <ListItemText primary="Dashboard" />
             </ListItem>
-            </Link>
-            <Divider />
-           <Link name="contact" onClick={handleDrawerClose}  className="link"to="/contact">
-            <ListItem button >
-              <ListItemIcon> <EmailIcon className="icon"/></ListItemIcon>
+          </Link>
+          <Divider />
+          <Link
+            name="contact"
+            onClick={handleDrawerClose}
+            className="link"
+            to="/contact"
+          >
+            <ListItem button>
+              <ListItemIcon>
+                {" "}
+                <EmailIcon className="icon" />
+              </ListItemIcon>
               <ListItemText primary="Manage Account" />
             </ListItem>
-            </Link>
-            <Divider />
-           
-            <Divider />
-            <ListItem style={{display: "flex", justifyContent: "center"}} >
-         
-            </ListItem>
-            <Divider />
-            
-            
-          
+          </Link>
+          <Divider />
+
+          <Divider />
+          <ListItem
+            style={{ display: "flex", justifyContent: "center" }}
+          ></ListItem>
+          <Divider />
         </List>
-        
       </Drawer>
     </header>
-
   );
 }
--- client/src/pages/Notifications/index.js
+++ client/src/pages/Notifications/index.js
@@ -66,7 +66,7 @@ export default function Notifications() {
               </button>
             </div>
           </div>
-        )) : <h4>No notifications</h4>}
+        )) : <h3>No notifications</h3>}
       </div>
     </div>
   );
