0a52fe548cf0d11c63cd7b59db1678ce651e31ac
Ben Hopkins <bjhops17@gmail.com>
2021-12-13 09:42:51 -0800

    removed some code I think was causing the issue

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ const postUser = require("./user-routes.js")
 
 router.use("/public/api", postUser)
 router.use("/protected/api", parseToken, protectedRoutes)
-router.use((req, res)=> {
-    res.status(500).send("No matching routes")
-})
+
 
 module.exports = router
